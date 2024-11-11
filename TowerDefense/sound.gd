54a8659a0b38da4303b1699c6ddc67e9cd26e8f6
kmm-2459355 <kmm2459355@stu.o-hara.ac.jp>
2024-11-11 12:16:34 +0900

    プロジェクトを追加

---
+++ TowerDefense/sound.gd
@@ -0,0 +1,22 @@
+extends Node
+class_name Sound
+
+@export var n_bgm : AudioStreamPlayer
+@export var n_se_list : Array[AudioStreamPlayer]
+#SEのid
+enum{
+	SE_HIT,
+}
+
+func _ready():
+	assert(n_bgm!=null)
+#BGMの再生
+func play_bgm():
+	n_bgm.play()	
+#BGM停止
+func stop_bgm():
+	n_bgm.stop()
+#SE再生
+func play_se(id):
+	if 0<=id and id < n_se_list.size():
+		n_se_list[id].play()
